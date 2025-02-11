a9fd051390d2fc6417083abc55c61121114a3b6f
Brenda Pereira <brendacamara@outlook.com>
2025-02-11 16:15:34 -0300

    feat: static, dynamic and layout metadatas

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,15 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `Browse all our ${numberOfPosts} Posts`,
+    description: `Check out the ${numberOfPosts} posts from the community.`,
+  };
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
